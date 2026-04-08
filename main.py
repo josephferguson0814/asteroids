9352d892b64554b4ac7fc94a04ec9a7340e3099f
josephferguson0814 <jferguson0814@gmail.com>
2026-04-08 12:21:21 -0400

    Made drawable and updateable containers to make drawing and updating sprites easier

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     Player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
-        Player1.update(dt)
+        updatable.update(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        Player1.draw(screen)
+        for item in drawable:
+            item.draw(screen)
+        #Player1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
